7067f50d9c02f0a65cc741a548dbde957881e91a
washington-shoji <shoji.washington@gmail.com>
2022-10-08 14:10:02 +1100

    first commit

---
+++ src/middleware/ValidateSchema.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi, { ObjectSchema } from 'joi';
+import Logging from '../library/Logging';
+
+export const ValidateSchema = (schema: ObjectSchema) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await schema.validateAsync(req.body);
+
+            next();
+        } catch (error) {
+            Logging.error(error);
+            return res.status(422).json({ error });
+        }
+    };
+};
---
+++ src/server.ts
@@ -0,0 +1,77 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import Logging from './library/Logging';
+
+const app = express();
+
+/**
+ * Connect to MongoDB
+ */
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Connected to MongoDb');
+        StartServer();
+    })
+    .catch((error) => {
+        Logging.error('Unable to connected to MongoDb');
+        Logging.error(error);
+    });
+
+/**
+ * Only start the server if Mongo Connects
+ */
+
+const StartServer = () => {
+    app.use((req, res, next) => {
+        /**
+         * Log the Request
+         */
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /**
+             * Log the Response
+             */
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    /** Rules of our API */
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+
+    /** Healthcheck */
+    app.get('/ping', (req, res, next) => res.status(200).json({ hello: 'world' }));
+
+    /** Error handling */
+    app.use((req, res, next) => {
+        const error = new Error('Not found');
+
+        Logging.error(error);
+
+        res.status(404).json({
+            message: error.message
+        });
+    });
+
+    http.createServer(app).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};
