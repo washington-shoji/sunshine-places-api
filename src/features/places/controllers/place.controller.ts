474054ec2de33d3e13c476dd1d57bd77e205de7c
washington-shoji <shoji.washington@gmail.com>
2022-10-10 20:36:35 +1100

    refactor(controller): updated controller with conditionals

--- src/features/places/controllers/place.controller.ts
+++ src/features/places/controllers/place.controller.ts
@@ -4,22 +4,27 @@ import { createPlaceService, getAllPlacesService, deletePlaceService, updatePlac
 
 export async function createPlaceController(req: Request, res: Response) {
     const body: IPlace = req.body;
-    const place = await createPlaceService(body);
-
-    if (place) {
-        return res.status(200).send(place);
-    } else {
-        return res.status(500).send({ message: 'Could not create a Place' });
+    try {
+        const place = await createPlaceService(body);
+        if (place) {
+            return res.status(200).send({ message: 'Place created successfully', data: place });
+        }
+    } catch (error) {
+        return res.status(500).send({ message: 'Could not create a Place', error: error });
     }
 }
 
 export async function getAllPlacesController(req: Request, res: Response) {
-    const allPlaces = await getAllPlacesService();
+    try {
+        const allPlaces = await getAllPlacesService();
 
-    if (allPlaces) {
-        return res.status(200).send(allPlaces);
-    } else {
-        return res.status(500).send({ message: 'Could not fetch Places' });
+        if (allPlaces && allPlaces.length > 0) {
+            return res.status(200).send({ message: 'Places fetched successfully', data: allPlaces });
+        } else {
+            return res.status(200).send({ message: 'There is no place yes', data: [] });
+        }
+    } catch (error) {
+        return res.status(500).send({ message: 'Could not fetch Places', error: error });
     }
 }
 
@@ -27,22 +32,31 @@ export async function updatePlaceByIdController(req: Request, res: Response) {
     const id: string = req.params.placeId;
     const body: IPlace = req.body;
 
-    const updatedPlace = await updatePlaceService(id, body);
+    try {
+        const updatedPlace = await updatePlaceService(id, body);
 
-    if (updatedPlace) {
-        res.status(202).send(updatedPlace);
-    } else {
-        return res.status(500).send({ message: 'Could not update Place' });
+        if (updatedPlace) {
+            return res.status(202).send({ message: 'Place updated successfully', data: updatedPlace });
+        } else {
+            return res.status(404).send({ message: 'Could not find place' });
+        }
+    } catch (error) {
+        return res.status(500).send({ message: 'Could not update Place', error: error });
     }
 }
 
 export async function deletePlaceByIdController(req: Request, res: Response) {
     const id: string = req.params.placeId;
-    const deletePlace = await deletePlaceService(id);
 
-    if (deletePlace) {
-        res.status(200).send({ message: 'Place successfully deleted' });
-    } else {
-        return res.status(500).send({ message: 'Could not delete Place' });
+    try {
+        const deletePlace = await deletePlaceService(id);
+
+        if (deletePlace) {
+            return res.status(202).send({ message: 'Place deleted successfully' });
+        } else {
+            return res.status(404).send({ message: 'Could not find place' });
+        }
+    } catch (error) {
+        return res.status(500).send({ message: 'Could not delete Place', error: error });
     }
 }
