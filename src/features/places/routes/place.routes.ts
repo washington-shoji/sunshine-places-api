d7e79e8b275d970b1cbfa3141b44d6e74a9d3759
washington-shoji <shoji.washington@gmail.com>
2022-10-10 20:59:08 +1100

    feat(route): added validator to the place router

--- src/features/places/routes/place.routes.ts
+++ src/features/places/routes/place.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { ValidateSchema } from '../../../middleware/ValidateSchema';
 import { createPlaceController, deletePlaceByIdController, getAllPlacesController, updatePlaceByIdController } from '../controllers/place.controller';
+import { Schemas } from '../validation-schema/schema.validation';
 
 export const placeRouter = express.Router();
 
-placeRouter.post('/place/', createPlaceController);
+placeRouter.post('/place/', ValidateSchema(Schemas.place.create), createPlaceController);
 placeRouter.get('/place/', getAllPlacesController);
-placeRouter.patch('/place/:placeId', updatePlaceByIdController);
+placeRouter.patch('/place/:placeId', ValidateSchema(Schemas.place.update), updatePlaceByIdController);
 placeRouter.delete('/place/:placeId', deletePlaceByIdController);
