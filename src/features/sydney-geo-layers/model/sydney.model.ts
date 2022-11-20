c702f279167924f9d4526cc4aa9bc0471a78f9de
washington-shoji <shoji.washington@gmail.com>
2022-11-20 11:37:42 +1100

    feat(model): added sydney layer model

---
+++ src/features/sydney-geo-layers/model/sydney.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { ILayer } from '../interface/sydneyLayer.interface';
+export interface ILayerModel extends ILayer, Document {}
+
+const polygonSchema = new Schema({
+    type: {
+        type: String,
+        enum: ['Polygon'],
+        required: true
+    },
+    coordinates: {
+        type: [[[Number]]],
+        required: true
+    }
+});
+
+const LayerSchema: Schema = new Schema(
+    {
+        suburb: String,
+        location: polygonSchema
+    },
+    { timestamps: true }
+);
+
+LayerSchema.index({ location: '2dsphere' });
+
+export const LayerModel = mongoose.model<ILayerModel>('Layer', LayerSchema);
