9a718678bd7c473a9b627165849d8eede07a04c5
washington-shoji <shoji.washington@gmail.com>
2022-11-20 11:36:28 +1100

    refactor(router): refactored place router

--- src/features/places/routes/place.routes.ts
+++ src/features/places/routes/place.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { ValidateSchema } from '../../../middleware/ValidateSchema';
-import { createPlaceController, deletePlaceByIdController, getAllPlacesController, updatePlaceByIdController } from '../controllers/place.controller';
+import { PlaceController } from '../controllers/place.controller';
 import { Schemas } from '../validation-schema/schema.validation';
 
 export const placeRouter = express.Router();
 
-placeRouter.post('/place/', ValidateSchema(Schemas.place.create), createPlaceController);
-placeRouter.get('/place/', getAllPlacesController);
-placeRouter.patch('/place/:placeId', ValidateSchema(Schemas.place.update), updatePlaceByIdController);
-placeRouter.delete('/place/:placeId', deletePlaceByIdController);
+placeRouter.post('/place/', ValidateSchema(Schemas.place.create), PlaceController.createPlace);
+placeRouter.get('/place/', PlaceController.getAllPlaces);
+placeRouter.patch('/place/:placeId', ValidateSchema(Schemas.place.update), PlaceController.updatePlaceById);
+placeRouter.delete('/place/:placeId', PlaceController.deletePlaceById);
