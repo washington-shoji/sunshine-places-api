8eafecf827518beaed6ebbd39acc3d0c59df7b05
washington-shoji <shoji.washington@gmail.com>
2023-01-04 21:49:27 +1100

    feat(interface): updated gis data interfaces

--- src/interface/interface.interface.ts
+++ src/interface/interface.interface.ts
@@ -6,9 +6,14 @@ export interface IError {
     };
 }
 
-export interface IGeometry {
+export interface IGeo_point_2d {
     geometry: {
         type: string;
         coordinates: number[];
     };
+}
+
+export interface IGeo_point_3d {
+    suburb: string;
+    location: { type: string; coordinates: [[[number]]] };
 }
