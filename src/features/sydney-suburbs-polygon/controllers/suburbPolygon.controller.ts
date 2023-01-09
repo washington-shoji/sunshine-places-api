ff620fa9c21e7ce76ecb0d9ca3849c19d51ca4f1
washington-shoji <shoji.washington@gmail.com>
2023-01-10 08:44:23 +1100

    feat(controller): added suburb polygon controller

--- src/features/sydney-suburbs-polygon/controllers/suburbPolygon.controller.ts
+++ src/features/sydney-suburbs-polygon/controllers/suburbPolygon.controller.ts
@@ -3,8 +3,33 @@ import { APIError } from '../../../library/classes/api-error';
 import { BaseError } from '../../../library/classes/base-error';
 import { SydneyPolygonLayerService } from '../service/suburbPolygonLayer.service';
 
+async function findAllPolygonLayer(req: Request, res: Response, next: NextFunction) {
+    try {
+        const result = await SydneyPolygonLayerService.findAllPolygonLayer();
+        return res.send(result);
+    } catch (error) {
+        const message = error instanceof APIError ? error.message : `Generic error for user`;
+        res.status((<BaseError>error)?.httpCode || 500).send(message);
+        next(error);
+    }
+}
+
+async function findNearPolygonLayer(req: Request, res: Response, next: NextFunction) {
+    const { longitude, latitude } = req.query;
+    const lon = parseFloat(parseFloat(longitude as string).toFixed(4));
+    const lat = parseFloat(parseFloat(latitude as string).toFixed(4));
+    try {
+        const result = await SydneyPolygonLayerService.findNearPolygonLayer(lon, lat);
+        return res.send(result);
+    } catch (error) {
+        const message = error instanceof APIError ? error.message : `Generic error for user`;
+        res.status((<BaseError>error)?.httpCode || 500).send(message);
+        next(error);
+    }
+}
+
 async function findPolygonLayerBySuburb(req: Request, res: Response, next: NextFunction) {
-    const suburb: string = req.body.suburb;
+    const suburb: string = req.query.suburb as string;
     try {
         const result = await SydneyPolygonLayerService.findPolygonLayerBySuburb(suburb);
         return res.send(result);
@@ -16,7 +41,7 @@ async function findPolygonLayerBySuburb(req: Request, res: Response, next: NextF
 }
 
 async function findPolygonLayerByPostcode(req: Request, res: Response, next: NextFunction) {
-    const postcode: string = req.body.postcode;
+    const postcode: string = req.query.postcode as string;
     try {
         const result = await SydneyPolygonLayerService.findPolygonLayerByPostcode(postcode);
         return res.send(result);
@@ -28,6 +53,8 @@ async function findPolygonLayerByPostcode(req: Request, res: Response, next: Nex
 }
 
 export const SydneyPolygonLayerController = {
+    findAllPolygonLayer,
+    findNearPolygonLayer,
     findPolygonLayerBySuburb,
     findPolygonLayerByPostcode
 };
