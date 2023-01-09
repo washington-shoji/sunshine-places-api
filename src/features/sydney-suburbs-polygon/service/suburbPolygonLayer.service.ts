baa69098740d69948cf30009f19925f84202c0dc
washington-shoji <shoji.washington@gmail.com>
2023-01-10 08:44:05 +1100

    feat(service): added suburb polygon service

--- src/features/sydney-suburbs-polygon/service/suburbPolygonLayer.service.ts
+++ src/features/sydney-suburbs-polygon/service/suburbPolygonLayer.service.ts
@@ -3,6 +3,40 @@ import { BaseError } from '../../../library/classes/base-error';
 import { HttpStatusCode } from '../../../library/enums/http.enum';
 import { SuburbPolygonLayerRepository } from '../repository/suburbPolygonLayerRepository';
 
+async function findAllPolygonLayer() {
+    try {
+        const result = await SuburbPolygonLayerRepository.findAllPolygonLayer();
+        if (!result) {
+            const errMessage = `Could not find data.`;
+            throw new APIError(errMessage, 'findInDB', HttpStatusCode.NOT_FOUND);
+        }
+        return result;
+    } catch (error) {
+        if (error instanceof APIError) {
+            throw error;
+        } else {
+            throw new BaseError('Could not perform async operation', error, 'findInDB');
+        }
+    }
+}
+
+async function findNearPolygonLayer(longitude: number, latitude: number) {
+    try {
+        const result = await SuburbPolygonLayerRepository.findNearPolygonLayer(longitude, latitude);
+        if (!result) {
+            const errMessage = `Could not find ${longitude} and ${latitude} data.`;
+            throw new APIError(errMessage, 'findInDB', HttpStatusCode.NOT_FOUND);
+        }
+        return result;
+    } catch (error) {
+        if (error instanceof APIError) {
+            throw error;
+        } else {
+            throw new BaseError('Could not perform async operation', error, 'findInDB');
+        }
+    }
+}
+
 async function findPolygonLayerBySuburb(suburb: string) {
     try {
         const result = await SuburbPolygonLayerRepository.findPolygonLayerBySuburb(suburb);
@@ -38,6 +72,8 @@ async function findPolygonLayerByPostcode(postcode: string) {
 }
 
 export const SydneyPolygonLayerService = {
+    findAllPolygonLayer,
+    findNearPolygonLayer,
     findPolygonLayerBySuburb,
     findPolygonLayerByPostcode
 };
