2f518fc79e9b4096287a3b8514a73430a922a7fb
washington-shoji <shoji.washington@gmail.com>
2023-01-10 08:44:40 +1100

    feat(route): added suburb polygon route

--- src/features/sydney-suburbs-polygon/routes/suburbPolygonLayer.routes.ts
+++ src/features/sydney-suburbs-polygon/routes/suburbPolygonLayer.routes.ts
@@ -3,5 +3,7 @@ import { SydneyPolygonLayerController } from '../controllers/suburbPolygon.contr
 
 export const sydneyPolygonLayerRouter = express.Router();
 
-sydneyPolygonLayerRouter.get('/polygon/suburb', SydneyPolygonLayerController.findPolygonLayerBySuburb);
+sydneyPolygonLayerRouter.get('/polygon/suburb', SydneyPolygonLayerController.findAllPolygonLayer);
+sydneyPolygonLayerRouter.get('/polygon/near', SydneyPolygonLayerController.findNearPolygonLayer);
+sydneyPolygonLayerRouter.get('/polygon/suburb-name', SydneyPolygonLayerController.findPolygonLayerBySuburb);
 sydneyPolygonLayerRouter.get('/polygon/postcode', SydneyPolygonLayerController.findPolygonLayerByPostcode);
